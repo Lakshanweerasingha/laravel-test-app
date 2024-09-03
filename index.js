9c76503ebbef1eaedb4f462e3886d90911d712b0
lakshanweerasingha <lakshanmaduwantha03@gmail.com>
2024-09-03 14:37:54 +0530

    test parts functions

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let abcd = ["lakshan", "maduwantha"];
+console.log(abcd.lastIndexOf());
+
+function great(name, age) {
+console.log('hello ' + name + ' and age is ' + age);
+}
+
+great('lakshan',23);
+
+function test(num1,num2){
+    return num1*num2;
+}
+
+let a = test(2,3);
+console.log(a);
